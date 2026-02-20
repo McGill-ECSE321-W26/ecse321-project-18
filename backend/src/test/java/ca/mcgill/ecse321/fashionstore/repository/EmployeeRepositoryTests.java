9af86db89ac96a7a3a81241c5d0eda749779fb8f
shenggaoyun <aurore.zhang@mail.mcgill.ca>
2026-02-19 21:58:56 -0500

    feat: test association employee and order

--- backend/src/test/java/ca/mcgill/ecse321/fashionstore/repository/EmployeeRepositoryTests.java
+++ backend/src/test/java/ca/mcgill/ecse321/fashionstore/repository/EmployeeRepositoryTests.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import ca.mcgill.ecse321.fashionstore.model.Employee;
+import ca.mcgill.ecse321.fashionstore.model.Order;
+import ca.mcgill.ecse321.fashionstore.model.Order.State;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Date;
 
 /**
  * @author Aurore Zhang (ororio0)
@@ -18,15 +21,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EmployeeRepositoryTests {
 
     @Autowired private EmployeeRepository employeeRepository;
+    @Autowired private OrderRepository orderRepository;
     private Employee employee;
+    private Order order;
 
     @BeforeEach
     // create an employee
     void createEmployee() {
-        String email = "employee@fashionstore.com";
-        String password = "password123";
-        String address = "123 Fashion Street";
-        int numLoyaltyPoints = 50;
+        String email = "ilovefashion@fashionstore.com";
+        String password = "P@ssw0rd";
+        String address = " 12 Building McConnell";
+        int numLoyaltyPoints = 8;
 
         Employee newEmployee = new Employee();
         newEmployee.setEmail(email);
@@ -36,6 +41,16 @@ class EmployeeRepositoryTests {
         // save the employee
         employeeRepository.save(newEmployee);
         employee = newEmployee;
+
+        // create an order and assign to employee
+        Order newOrder = new Order();
+        newOrder.setState(State.ASSIGNED);
+        newOrder.setOrderDate(Date.valueOf("2030-02-30"));
+        newOrder.setDeliveryAddress("67 McGill University Avenue");
+        newOrder.setPrice(67.88f);
+        newOrder.setEmployee(newEmployee);
+        orderRepository.save(newOrder);
+        order = newOrder;
     }
 
     /**
@@ -45,6 +60,7 @@ class EmployeeRepositoryTests {
      */
     @AfterEach
     void clearDatabase() {
+        orderRepository.deleteAll();
         employeeRepository.deleteAll();
     }
 
@@ -114,5 +130,20 @@ class EmployeeRepositoryTests {
                 employee.getNumLoyaltyPoints(),
                 employeeFromDb.getNumLoyaltyPoints(),
                 "Employee loyalty points are not saved in database.");
+    }
+    /**
+     * Test retrieval of employee's assigned order reference is correct.
+     *
+     * @author Aurore Zhang (ororio0)
+     */
+    @Test
+    void testPersistAndLoadEmployeeAssignedOrder() {
+        Order orderFromDb = orderRepository.findOrderById(order.getId());
+        assertNotNull(orderFromDb, "Could not find saved order in database.");
+        assertNotNull(orderFromDb.getEmployee(), "Order has no employee reference in database.");
+        assertEquals(
+                employee.getEmail(),
+                orderFromDb.getEmployee().getEmail(),
+                "Employee reference in order is not saved correctly in database.");
     }
 }
