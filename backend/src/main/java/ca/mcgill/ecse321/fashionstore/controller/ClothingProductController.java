a281f9c21cdf730aaa43e9b87a2583320439d77c
jenni4u <jennifer.you@mail.mcgill.ca>
2026-03-06 00:32:42 -0500

    feat: endpoints for - creating a new product - creating a new clothing item - updating an existing product

--- backend/src/main/java/ca/mcgill/ecse321/fashionstore/controller/ClothingProductController.java
+++ backend/src/main/java/ca/mcgill/ecse321/fashionstore/controller/ClothingProductController.java
@@ -1,1 +1,62 @@
+package ca.mcgill.ecse321.fashionstore.controller;
 
+import ca.mcgill.ecse321.fashionstore.dto.ClothingProductRequestDto;
+import ca.mcgill.ecse321.fashionstore.dto.ClothingProductResponseDto;
+import ca.mcgill.ecse321.fashionstore.service.ClothingProductService;
+import org.apache.logging.log4j.internal.annotation.SuppressFBWarnings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Controller for ClothingProduct REST API endpoints */
+@RestController
+@SuppressFBWarnings("SPRING_ENDPOINT")
+public class ClothingProductController {
+    private ClothingProductService clothingProductService;
+
+    /**
+     * Constructor for ClothingProductController
+     *
+     * @param clothingProductService clothing product service class
+     * @author Jennifer You (jenni4u)
+     */
+    @Autowired
+    public ClothingProductController(ClothingProductService clothingProductService) {
+        this.clothingProductService = clothingProductService;
+    }
+
+    /**
+     * Creates a new clothing product
+     *
+     * @param clothingProductRequestDto the details of the new clothing product to create
+     * @return a ClothingProductResponseDto
+     * @author Jennifer You (jenni4u)
+     */
+    @PostMapping("/fashionstore/clothingproduct")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClothingProductResponseDto createClothingProduct(
+            @RequestBody ClothingProductRequestDto clothingProductRequestDto) {
+        return clothingProductService.createClothingProduct(clothingProductRequestDto);
+    }
+
+    /**
+     * Updates an existing clothing product
+     *
+     * @param clothingProductRequestDto the details of the clothing product to update
+     * @param id the id of the clothing product to update
+     * @return a ClothingProductResponseDto
+     * @author Jennifer You (jenni4u)
+     */
+    @PutMapping("/fashionstore/clothingproduct/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ClothingProductResponseDto updateClothingProduct(
+            @RequestBody ClothingProductRequestDto clothingProductRequestDto,
+            @PathVariable int id) {
+        return clothingProductService.updateClothingProduct(clothingProductRequestDto, id);
+    }
+}
--- backend/src/main/java/ca/mcgill/ecse321/fashionstore/service/ClothingProductService.java
+++ backend/src/main/java/ca/mcgill/ecse321/fashionstore/service/ClothingProductService.java
@@ -1,1 +1,74 @@
+package ca.mcgill.ecse321.fashionstore.service;
 
+import ca.mcgill.ecse321.fashionstore.dto.ClothingProductRequestDto;
+import ca.mcgill.ecse321.fashionstore.dto.ClothingProductResponseDto;
+import ca.mcgill.ecse321.fashionstore.model.ClothingProduct;
+import ca.mcgill.ecse321.fashionstore.repository.ClothingProductRepository;
+import jakarta.validation.Valid;
+import org.apache.logging.log4j.internal.annotation.SuppressFBWarnings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+/** Service class for ClothingProduct. */
+@Service
+@Validated
+public class ClothingProductService {
+    private ClothingProductRepository clothingProductRepository;
+
+    /**
+     * Constructor for ClothingProductService class
+     *
+     * @param clothingProductRepository clothing product repository class
+     * @author Jennifer You (jenni4u)
+     */
+    @Autowired
+    @SuppressFBWarnings("EI_EXPOSE_REP2")
+    public ClothingProductService(ClothingProductRepository clothingProductRepository) {
+        this.clothingProductRepository = clothingProductRepository;
+    }
+
+    /**
+     * Service method to create a new clothing product
+     *
+     * @param clothingProductRequestDto Clothing Product Request DTO
+     * @return Clothing Product Response DTO
+     * @author Jennifer You (jenni4u)
+     */
+    public ClothingProductResponseDto createClothingProduct(
+            @Valid ClothingProductRequestDto clothingProductRequestDto) {
+        // create new clothing product
+        ClothingProduct clothingProduct = new ClothingProduct();
+        clothingProduct.setName(clothingProductRequestDto.name());
+        clothingProduct.setPrice(clothingProductRequestDto.price());
+        clothingProduct.setImage(clothingProductRequestDto.image());
+
+        // save clothing product to repository
+        clothingProduct = this.clothingProductRepository.save(clothingProduct);
+        return new ClothingProductResponseDto(clothingProduct);
+    }
+
+    /**
+     * Service method to update an existing clothing product
+     *
+     * @param clothingProductRequestDto Clothing Product Request DTO
+     * @param clothingProductId ID of the clothing product to update
+     * @return Clothing Product Response DTO
+     * @author Jennifer You (jenni4u)
+     */
+    public ClothingProductResponseDto updateClothingProduct(
+            @Valid ClothingProductRequestDto clothingProductRequestDto, int clothingProductId) {
+        // find clothing product to update
+        ClothingProduct clothingProduct =
+                Utils.findClothingProductById(clothingProductRepository, clothingProductId);
+
+        // update clothing product details
+        clothingProduct.setName(clothingProductRequestDto.name());
+        clothingProduct.setPrice(clothingProductRequestDto.price());
+        clothingProduct.setImage(clothingProductRequestDto.image());
+
+        // save updated clothing product to repository
+        clothingProduct = this.clothingProductRepository.save(clothingProduct);
+        return new ClothingProductResponseDto(clothingProduct);
+    }
+}
