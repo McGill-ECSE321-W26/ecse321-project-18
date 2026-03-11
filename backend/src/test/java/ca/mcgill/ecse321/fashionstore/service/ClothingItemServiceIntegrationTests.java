6d853d746f0fd849b187e89418117f62c4a5cff5
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-03-11 17:34:26 -0400

    feat: clothingitem integration tests

--- backend/src/test/java/ca/mcgill/ecse321/fashionstore/service/ClothingItemServiceIntegrationTests.java
+++ backend/src/test/java/ca/mcgill/ecse321/fashionstore/service/ClothingItemServiceIntegrationTests.java
@@ -1,27 +1,59 @@
 package ca.mcgill.ecse321.fashionstore.service;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import ca.mcgill.ecse321.fashionstore.controller.ClothingProductController;
-import ca.mcgill.ecse321.fashionstore.dto.ClothingProductRequestDto;
+import ca.mcgill.ecse321.fashionstore.dto.ClothingItemResponseDto;
+import ca.mcgill.ecse321.fashionstore.model.ClothingItem;
+import ca.mcgill.ecse321.fashionstore.model.ClothingProduct;
 import ca.mcgill.ecse321.fashionstore.repository.ClothingItemRepository;
 import ca.mcgill.ecse321.fashionstore.repository.ClothingProductRepository;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
 /** Integration test suite for ClothingItemService. */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureRestTestClient
 class ClothingItemServiceIntegrationTests {
+    @Autowired RestTestClient client;
     @Autowired ClothingProductController clothingProductController;
     @Autowired private ClothingProductRepository clothingProductRepository;
     @Autowired private ClothingItemRepository clothingItemRepository;
+
+    private ClothingProduct clothingProduct;
+    private ClothingItem clothingItem;
+    private static final String clothingProductUri = "/fashionstore/clothingproduct/{id}";
+    private static final String clothingItemUri =
+            "/fashionstore/clothingproduct/{productId}/clothingitem/{itemId}";
+
+    /** Setup method for the test suite. */
+    @BeforeAll
+    void createClothingProducts() {
+        // set clothing product first
+        ClothingProduct clothingProduct = new ClothingProduct();
+        clothingProduct.setImage("hoodie.png");
+        clothingProduct.setPrice(69.99f);
+        clothingProduct.setName("Hoodie");
+        clothingProductRepository.save(clothingProduct);
+        this.clothingProduct = clothingProduct;
+
+        // create first clothing item
+        ClothingItem clothingItem = new ClothingItem();
+        clothingItem.setClothingProduct(clothingProduct);
+        clothingItem.setSize(ClothingItem.Size.M);
+        clothingItem.setColour(ClothingItem.Colour.YELLOW);
+        clothingItem.setNumInStock(100);
+        clothingItemRepository.save(clothingItem);
+        this.clothingItem = clothingItem;
+    }
 
     /** Teardown method for test suite. (placeholder, please modify if needed) */
     @AfterAll
@@ -30,12 +62,31 @@ class ClothingItemServiceIntegrationTests {
         clothingProductRepository.deleteAll();
     }
 
-    /** Test that a clothing item exists. (placeholder, please modify if needed) */
+    /**
+     * Test successful GET for a ClothingItem.
+     *
+     * @author Qiuyu Huang (redacted24)
+     */
     @Test
-    void test1() {
-        ClothingProductRequestDto request =
-                new ClothingProductRequestDto("hoodie", 69.99f, "hoodie.png");
-        assertDoesNotThrow(
-                () -> clothingProductController.createClothingProduct(request), "should not throw");
+    void getClothingItemOk() {
+        int productId = clothingProduct.getId();
+        int itemId = clothingItem.getId();
+        ClothingItemResponseDto response =
+                client.get()
+                        .uri(clothingItemUri, productId, itemId)
+                        .exchange()
+                        .expectStatus()
+                        .isOk()
+                        .expectBody(ClothingItemResponseDto.class)
+                        .returnResult()
+                        .getResponseBody();
+
+        assertNotNull(response, "Response body for GET ClothingItem is null.");
+        assertEquals(
+                response.size(),
+                clothingItem.getSize(),
+                "Wrong size for response of GET ClothingItem.");
+        assertEquals(
+                response.id(), clothingItem.getId(), "Wrong Id for response of GET ClothingItem.");
     }
 }
