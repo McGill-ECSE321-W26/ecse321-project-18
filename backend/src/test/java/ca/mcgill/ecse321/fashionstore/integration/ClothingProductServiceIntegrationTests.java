c859987229edd4fdcf04fc9234d5c72c95fb7e9a
Carolyn Wu <carolynrl.wu@gmail.com>
2026-03-14 02:18:53 -0400

    chore: integration tests for get matching clothing products

--- backend/src/test/java/ca/mcgill/ecse321/fashionstore/integration/ClothingProductServiceIntegrationTests.java
+++ backend/src/test/java/ca/mcgill/ecse321/fashionstore/integration/ClothingProductServiceIntegrationTests.java
@@ -1,8 +1,11 @@
 package ca.mcgill.ecse321.fashionstore.integration;
 
+import static ca.mcgill.ecse321.fashionstore.dto.ClothingProductResponseDto.clothingProductsToResponseDtos;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ca.mcgill.ecse321.fashionstore.controller.ClothingProductController;
 import ca.mcgill.ecse321.fashionstore.dto.ClothingItemResponseDto;
@@ -18,7 +21,11 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 /** Integration test suite for ClothingProductService. */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,9 +38,13 @@ class ClothingProductServiceIntegrationTests {
     @Autowired private ClothingItemRepository clothingItemRepository;
 
     private ClothingProduct clothingProduct;
+    private ClothingProduct clothingProduct2;
+    private List<ClothingProduct> allClothingProducts;
     private static final String clothingProductUri = "/fashionstore/clothingproduct/{id}";
+    private static final String clothingProductsUri = "/fashionstore/clothingproduct";
 
     private ClothingItem clothingItem;
+    private ClothingItem clothingItem2;
     private static final String clothingItemUri =
             "/fashionstore/clothingproduct/{productId}/clothingitem/{itemId}";
 
@@ -56,6 +67,25 @@ class ClothingProductServiceIntegrationTests {
         clothingItem.setNumInStock(100);
         clothingItemRepository.save(clothingItem);
         this.clothingItem = clothingItem;
+
+        // create another clothing product
+        ClothingProduct clothingProduct2 = new ClothingProduct();
+        clothingProduct2.setImage("sweater.png");
+        clothingProduct2.setPrice(55.55f);
+        clothingProduct2.setName("Sweater");
+        clothingProductRepository.save(clothingProduct2);
+        this.clothingProduct2 = clothingProduct2;
+
+        // another clothing item
+        ClothingItem clothingItem2 = new ClothingItem();
+        clothingItem2.setClothingProduct(clothingProduct2);
+        clothingItem2.setSize(ClothingItem.Size.XL);
+        clothingItem2.setColour(ClothingItem.Colour.RED);
+        clothingItem2.setNumInStock(50);
+        clothingItemRepository.save(clothingItem2);
+        this.clothingItem2 = clothingItem2;
+
+        this.allClothingProducts = List.of(clothingProduct, clothingProduct2);
     }
 
     /** Teardown method for test suite. (placeholder, please modify if needed) */
@@ -115,5 +145,112 @@ class ClothingProductServiceIntegrationTests {
         assertNull(
                 response.name(),
                 "Response name should be null for a failed GET for clothing product.");
+    }
+
+    /**
+     * Test retrieving all clothing products by providing no search and no filters.
+     *
+     * @author Carolyn Wu (cw118)
+     */
+    @Test
+    void testGetAllClothingProducts() {
+        List<ClothingProductResponseDto> expected = clothingProductsToResponseDtos(this.allClothingProducts);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(clothingProductsUri);
+        List<ClothingProductResponseDto> response =
+            getMatchingClothingProductsResponseBody(builder);
+
+        assertNotNull(response, "Response body for get all clothing products is null.");
+        assertIterableEquals(expected, response, "Not all clothing products were retrieved correctly.");
+    }
+
+    /**
+     * Test finding no matching clothing products for given search.
+     *
+     * @author Carolyn Wu (cw118)
+     */
+    @Test
+    void testSearchNoMatchingClothingProducts() {
+        UriComponentsBuilder builder = UriComponentsBuilder
+            .fromUriString(clothingProductsUri)
+            .queryParam("name", "strawberry");
+
+        List<ClothingProductResponseDto> response =
+            getMatchingClothingProductsResponseBody(builder);
+
+        assertNoMatchingClothingProducts(response);
+    }
+
+    /**
+     * Test finding no matching clothing products for given size filters.
+     *
+     * @author Carolyn Wu (cw118)
+     */
+    @Test
+    void testFilterSizeNoMatchingClothingProducts() {
+        UriComponentsBuilder builder = UriComponentsBuilder
+            .fromUriString(clothingProductsUri)
+            .queryParam("sizes", List.of(ClothingItem.Size.S, ClothingItem.Size.L));
+
+        List<ClothingProductResponseDto> response =
+            getMatchingClothingProductsResponseBody(builder);
+
+        assertNoMatchingClothingProducts(response);
+    }
+
+    /**
+     * Test finding no matching clothing products for given colour filters.
+     *
+     * @author Carolyn Wu (cw118)
+     */
+    @Test
+    void testFilterColourNoMatchingClothingProducts() {
+        UriComponentsBuilder builder = UriComponentsBuilder
+            .fromUriString(clothingProductsUri)
+            .queryParam("colours", List.of(ClothingItem.Colour.BLUE, ClothingItem.Colour.PURPLE));
+
+        List<ClothingProductResponseDto> response =
+            getMatchingClothingProductsResponseBody(builder);
+
+        assertNoMatchingClothingProducts(response);
+    }
+
+    /**
+     * Test retrieving clothing product(s) that match(es) given search and filters.
+     *
+     * @author Carolyn Wu (cw118)
+     */
+    @Test
+    void testMatchingClothingProducts() {
+        List<ClothingProductResponseDto> expected = List.of(new ClothingProductResponseDto(clothingProduct));
+
+        UriComponentsBuilder builder = UriComponentsBuilder
+            .fromUriString(clothingProductsUri)
+            .queryParam("name", "hood")
+            .queryParam("sizes", List.of(ClothingItem.Size.M))
+            .queryParam("colours", List.of(ClothingItem.Colour.YELLOW, ClothingItem.Colour.RED));
+
+        List<ClothingProductResponseDto> response =
+            getMatchingClothingProductsResponseBody(builder);
+
+        assertNotNull(response, "Response body for get matching clothing products is null.");
+        assertIterableEquals(expected, response, "Matching clothing products are incorrect.");
+    }
+
+    private List<ClothingProductResponseDto> getMatchingClothingProductsResponseBody(UriComponentsBuilder builder) {
+        return
+            client.get()
+                .uri(builder.build().encode().toUri())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<List<ClothingProductResponseDto>>() {})
+                .returnResult()
+                .getResponseBody();
+    }
+
+    private void assertNoMatchingClothingProducts(List<ClothingProductResponseDto> response) {
+        assertNotNull(response, "Response body for no matching clothing products should not be null.");
+        assertTrue(response.isEmpty(), "List of matching clothing products is not empty.");
     }
 }
