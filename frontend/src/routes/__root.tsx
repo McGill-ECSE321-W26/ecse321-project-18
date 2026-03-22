b5a5f3c0e87148710848a3d3cca07f675d6837f0
Carolyn Wu <carolynrl.wu@gmail.com>
2026-03-22 02:51:27 -0400

    chore(frontend): build workflow, start TopNav, doc head management

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,17 +1,36 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
+
+import TopNav from "#/components/TopNav";
+import Footer from "#/components/Footer";
 
 import "../styles.css";
 
 export const Route = createRootRoute({
-  component: RootComponent,
-});
-
-function RootComponent() {
-  return (
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        content: "Fashion store application",
+      },
+      { title: "Fashion Store" },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
+  }),
+  component: () => (
     <>
+      <HeadContent />
+
+      <TopNav />
       <Outlet />
+      <Footer />
+
       <TanStackDevtools
         config={{
           position: "bottom-right",
@@ -24,5 +43,5 @@ function RootComponent() {
         ]}
       />
     </>
-  );
-}
+  ),
+});
