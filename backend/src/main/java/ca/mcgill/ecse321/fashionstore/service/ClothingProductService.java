fb8b9bf2ad3238155d69e5b35ed1439ba96d1497
Kenneth Wang <160696438+KennethWang6@users.noreply.github.com>
2026-03-30 22:05:40 -0400

    checkstyle fix

--- backend/src/main/java/ca/mcgill/ecse321/fashionstore/service/ClothingProductService.java
+++ backend/src/main/java/ca/mcgill/ecse321/fashionstore/service/ClothingProductService.java
@@ -1,13 +1,5 @@
 package ca.mcgill.ecse321.fashionstore.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.logging.log4j.internal.annotation.SuppressFBWarnings;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
-
 import ca.mcgill.ecse321.fashionstore.dto.ClothingProductRequestDto;
 import ca.mcgill.ecse321.fashionstore.exception.FashionStoreException;
 import ca.mcgill.ecse321.fashionstore.model.ClothingItem;
@@ -15,6 +7,12 @@ import ca.mcgill.ecse321.fashionstore.model.ClothingProduct;
 import ca.mcgill.ecse321.fashionstore.repository.ClothingProductRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.logging.log4j.internal.annotation.SuppressFBWarnings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 /** Service class for ClothingProduct. */
 @Service
