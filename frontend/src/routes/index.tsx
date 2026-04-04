fddd2c880241f6bcd63d5bec990a5328dcb275c5
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-04-03 21:50:06 -0400

    refactor: cleaned up root and index

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
+import HomePage from "#/components/HomePage";
 
 export const Route = createFileRoute("/")({
   head: () => ({
     meta: [
       {
-        title: "Home | Stilton's Store",
+        title: "Stilton's Store",
       },
     ],
   }),
@@ -15,7 +16,7 @@ function App() {
   return (
     <>
       <main className="px-4 pb-8 pt-14">
-        <h1>Fashion store</h1>
+        <HomePage />
       </main>
     </>
   );
