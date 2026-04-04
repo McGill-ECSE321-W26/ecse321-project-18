b2d7cbfef387e7569af33605b23ebfbe08bd9179
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-04-03 21:21:45 -0400

    feat: proper text and call to action on 404 page

--- frontend/src/components/NotFoundPage.tsx
+++ frontend/src/components/NotFoundPage.tsx
@@ -1,3 +1,16 @@
 export default function NotFoundPage() {
-  return <div>not found :p</div>;
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl text-red-500 font-bold">404 Not Found Error</h1>
+      <br></br>
+      <p>
+        The page you requested could not be found. Please contact the
+        administrator for help if this is unexpected.
+      </p>
+      <br></br>
+      <p>
+        You can also click the Stilton's Store logo to go back to the home page.
+      </p>
+    </div>
+  );
 }
