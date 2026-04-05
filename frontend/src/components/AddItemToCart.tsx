36aab5143f1a3cc5cf28a2b5202be88a4a09781e
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-04-04 21:15:30 -0400

    feat: add to cart button

--- frontend/src/components/AddItemToCart.tsx
+++ frontend/src/components/AddItemToCart.tsx
@@ -1,6 +1,7 @@
-import { Card } from "@heroui/react";
+import { Button, Card, FieldError, Label, NumberField } from "@heroui/react";
 import { useMutation } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { useNavigate } from "@tanstack/react-router";
 import type {
   ClothingColour,
   ClothingItemResponse,
@@ -10,6 +11,7 @@ import type {
 } from "#/types/api";
 import { successToast } from "#/utils/helpers";
 import { postRequest } from "#/utils/httpClient";
+import { useAuth } from "#/auth";
 
 type AddItemToCartProps = {
   price: number;
@@ -18,48 +20,13 @@ type AddItemToCartProps = {
   isManager: boolean;
 };
 
-// POST to add cart endpoint
-// const addMutation = useMutation({
-//   mutationFn: async ({
-//     shoppingCartItemRequest,
-//   }: {
-//     shoppingCartItemRequest: ShoppingCartItemRequest;
-//   }): Promise<ShoppingCartResponse> =>
-//     await postRequest(
-//       `/account/customer/${customerId}/shoppingcartitem`,
-//       shoppingCartItemRequest,
-//     ),
-//   onSuccess: () => {
-//     successToast("Successfully added to cart!");
-//     navigate({ to: "/cart" });
-//   },
-// });
-
-// Handle adding to cart
-// const handleAddtoCart = async (cartItem: ShoppingCartItemRequest) => {
-//   try {
-//     await addMutation.mutateAsync({ shoppingCartItemRequest: cartItem });
-//   } catch (err) {}
-// };
-
-// create ShoppingCartItemRequest from ClothingItemResponse
-const createShoppingCartItemRequest = (
-  clothingItemResponse: ClothingItemResponse,
-  qty: number,
-) => {
-  const sc: ShoppingCartItemRequest = {
-    clothingItemId: clothingItemResponse.id,
-    quantity: qty,
-  };
-  return sc;
-};
-
 export default function AddItemToCart({
   price,
   colour,
   items,
   isManager,
 }: AddItemToCartProps) {
+  // Variables
   const areItemsAvailable = items && items.length > 0;
   const defaultSize = areItemsAvailable ? items[0].size : null;
   const defaultNumInStock = areItemsAvailable ? items[0].numInStock : 0;
@@ -69,61 +36,133 @@ export default function AddItemToCart({
   const [selectedNumInStock, setSelectedNumInStock] =
     useState<number>(defaultNumInStock);
   const [quantity, setQuantity] = useState<number>(1);
+  const customerId = useAuth().user?.id;
+  const navigate = useNavigate(); // for navigating to cart after adding item
 
   useEffect(() => {
     setSelectedSize(defaultSize);
     setSelectedNumInStock(defaultNumInStock);
   }, [colour, items]);
 
-  return (
-    <Card>
-      <h3 className="text-xl">
-        <strong>Price:</strong> ${price}
-      </h3>
-      <h3 className="text-xl">
-        <strong>Size:</strong> {selectedSize || "no size selected."}
-      </h3>
-      {areItemsAvailable ? (
-        <>
-          <div className="grid grid-cols-5 items-center gap-4">
-            {items.map((item) => {
-              const { size, numInStock } = item;
+  // POST to add cart endpoint
+  const addMutation = useMutation({
+    mutationFn: async ({
+      shoppingCartItemRequest,
+    }: {
+      shoppingCartItemRequest: ShoppingCartItemRequest;
+    }): Promise<ShoppingCartResponse> =>
+      await postRequest(
+        `/account/customer/${customerId}/shoppingcartitem`,
+        shoppingCartItemRequest,
+      ),
+    onSuccess: () => {
+      successToast("Successfully added to cart!");
+      navigate({ to: "/cart" });
+    },
+  });
 
-              return (
-                <p
-                  key={size}
-                  className={
-                    (selectedSize === size
-                      ? "font-bold border-2 border-blue-900 "
-                      : "") +
-                    "text-center p-4 bg-white shadow-sm shadow-gray-400 rounded-3xl hover:cursor-pointer hover:shadow-blue-400 hover:bg-blue-50 transition-all"
-                  }
-                  onClick={() => {
-                    setSelectedSize(size);
-                    setSelectedNumInStock(numInStock);
-                  }}
-                >
-                  {size}
-                </p>
-              );
-            })}
-          </div>
-          <h3 className="text-xl">
-            {selectedNumInStock !== 0 ? (
-              <>
-                <strong>In stock:</strong> {selectedNumInStock} remaining
-              </>
-            ) : (
-              <>
-                <span className="font-bold text-red-500">Out of stock.</span>{" "}
-                (You can still add this to your cart as a wishlist item.)
-              </>
-            )}
-          </h3>
-        </>
-      ) : (
-        <p>No items available.</p>
-      )}
-    </Card>
+  // Takes in size and quantity and
+  // returns a shoppingCartItemRequest to pass into handleAddToCart().
+  const createShoppingCartItemRequest = (
+    size: ClothingSize | null,
+    qty: number,
+  ) => {
+    if (size === null) {
+      const out: ShoppingCartItemRequest = { clothingItemId: -1, quantity: -1 };
+      return out;
+    }
+    const clothingItem = items?.find((u) => u.size === size);
+    let id = 0;
+    clothingItem !== undefined ? (id = clothingItem.id) : (id = 0);
+    const out: ShoppingCartItemRequest = { clothingItemId: id, quantity: qty };
+    return out;
+  };
+
+  // Handle adding to cart
+  const handleAddtoCart = async (cartItem: ShoppingCartItemRequest) => {
+    try {
+      await addMutation.mutateAsync({ shoppingCartItemRequest: cartItem });
+    } catch (err) {}
+  };
+
+  return (
+    <>
+      <Card>
+        <h3 className="text-xl">
+          <strong>Price:</strong> ${price}
+        </h3>
+        <h3 className="text-xl">
+          <strong>Size:</strong> {selectedSize || "no size selected."}
+        </h3>
+        {areItemsAvailable ? (
+          <>
+            <div className="grid grid-cols-5 items-center gap-4">
+              {items.map((item) => {
+                const { size, numInStock } = item;
+
+                return (
+                  <p
+                    key={size}
+                    className={
+                      (selectedSize === size
+                        ? "font-bold border-2 border-blue-900 "
+                        : "") +
+                      "text-center p-4 bg-white shadow-sm shadow-gray-400 rounded-3xl hover:cursor-pointer hover:shadow-blue-400 hover:bg-blue-50 transition-all"
+                    }
+                    onClick={() => {
+                      setSelectedSize(size);
+                      setSelectedNumInStock(numInStock);
+                    }}
+                  >
+                    {size}
+                  </p>
+                );
+              })}
+            </div>
+            <h3 className="text-xl">
+              {selectedNumInStock !== 0 ? (
+                <>
+                  <strong>In stock:</strong> {selectedNumInStock} remaining
+                  <div className="w-33">
+                    <NumberField
+                      className="w-full max-w-64"
+                      defaultValue={1}
+                      minValue={1}
+                      name="quantity"
+                      maxValue={selectedNumInStock}
+                      onChange={setQuantity}
+                      isDisabled={isManager}
+                    >
+                      <Label className="text-xl mt-2 font-bold">Quantity</Label>
+                      <NumberField.Group>
+                        <NumberField.DecrementButton />
+                        <NumberField.Input className="w-15" />
+                        <NumberField.IncrementButton />
+                      </NumberField.Group>
+                    </NumberField>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <span className="font-bold text-red-500">Out of stock.</span>{" "}
+                  (You can still add this to your cart as a wishlist item.)
+                </>
+              )}
+            </h3>
+          </>
+        ) : (
+          <p>No items available.</p>
+        )}
+      </Card>
+      <Button
+        onPress={() =>
+          handleAddtoCart(createShoppingCartItemRequest(selectedSize, quantity))
+        }
+        isDisabled={selectedNumInStock <= 0 || isManager}
+        className="mt-5"
+      >
+        {isManager ? <p>Cannot add as manager</p> : <p>Add to cart</p>}
+      </Button>
+    </>
   );
 }
