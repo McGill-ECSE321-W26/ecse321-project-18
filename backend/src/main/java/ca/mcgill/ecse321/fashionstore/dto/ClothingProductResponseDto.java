6162c42cc86ff720f15b6f1c17c17839d95d2ab3
jenni4u <jen.g.s.you@gmail.com>
2026-04-06 02:40:43 -0400

    fix: image is optional in creating product

--- backend/src/main/java/ca/mcgill/ecse321/fashionstore/dto/ClothingProductResponseDto.java
+++ backend/src/main/java/ca/mcgill/ecse321/fashionstore/dto/ClothingProductResponseDto.java
@@ -23,7 +23,7 @@ public record ClothingProductResponseDto(
         @NotNull(message = "ID must not be null.") @Positive(message = "ID must be positive.") Integer id,
         @NotBlank(message = "Name must not be blank.") String name,
         @NotNull(message = "Price must not be null.") @DecimalMin("0.01") Float price,
-        @NotBlank(message = "Image must not be blank.") String image,
+        String image,
         @NotNull(message = "List of ClothingItems must not be null.") @Size(min = 1, message = "Order must have at least one item.") List<@Valid ClothingItemResponseDto> clothingItems) {
 
     /** Constructor override for clothingItems as it sets a mutable reference. */
